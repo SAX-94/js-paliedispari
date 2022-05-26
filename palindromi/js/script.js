df87f180e243cdc6ec373c19e58b4a30a2cc579c
SAX_94 <matteosacchi94@gmail.com>
2022-05-26 17:33:28 +0200

    palindromi completed

---
+++ palindromi/js/script.js
@@ -0,0 +1,28 @@
+// L'utente inserisce una parola.
+const parola = prompt("Inserisci una parola");
+
+// La trasformo in una array composta dalle singole lettere
+let parolaDivisa = parola.split("");
+console.log(parolaDivisa);
+
+// Inverto l'ordine deglie elementi dell'array
+parolaDivisa = parolaDivisa.reverse();
+console.log(parolaDivisa);
+
+// Prendo gli elementi dell'array e li unisco a formare uan stringa
+let parolaInvertita = parolaDivisa.join("");
+console.log(parolaInvertita);
+
+// verifico se la parola inserita è palindroma
+if (parola == parolaInvertita) {
+    alert("La parola inserita è palindroma");
+} else {
+    alert("La parola inserita non è palindroma");
+}
+
+// Transformo il processo in una funzione
+
+function invertiParola(str){
+    let strInversa = str.split("").reverse().join("");
+    return strInversa;
+}
